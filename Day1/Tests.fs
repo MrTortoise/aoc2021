600229eb6a6f2ef552474716da75ecab0315831c
John Nicholas <john.nicholas@dinoroar.io>
2021-12-05 23:44:23 +0000

    incrementing done

--- Day1/Tests.fs
+++ Day1/Tests.fs
@@ -6,11 +6,12 @@ let countIncreases list =
     match list with
     | [] -> 0
     | [ a ] -> 0
+    | [a;b] -> if b>a then 1 else 0
     | _ -> 0
 
 [<Fact>]
 let ``empty list`` () =
-    // let data = [|199,200,208,210,200,207,240,269,260,263|]
+    // let data = [|199;200;208;210;200;207;240;269;260;263|]
     let count = countIncreases([])
     Assert.Equal(0, count)
     
@@ -21,10 +22,15 @@ let ``single item list`` () =
 
 [<Fact>]
 let ``2 item list same`` () =
-    let count = countIncreases([1,1])
+    let count = countIncreases([1;1])
     Assert.Equal(0, count)
 
 [<Fact>]
 let ``2 item list decreasing`` () =
-    let count = countIncreases([2,1])
+    let count = countIncreases([2;1])
     Assert.Equal(0, count)
+    
+[<Fact>]
+let ``2 item list increasing`` () =
+    let count = countIncreases([1;2])
+    Assert.Equal(1, count)
