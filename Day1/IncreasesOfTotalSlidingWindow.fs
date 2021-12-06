ee3856d437b311a4033f93fc07aa9146f793012b
John Nicholas <john.nicholas@dinoroar.io>
2021-12-06 12:12:21 +0000

    first increments of sum of sliding window test

---
+++ Day1/IncreasesOfTotalSlidingWindow.fs
@@ -0,0 +1,9 @@
+﻿module Day1.IncreasesOfTotalSlidingWindow
+
+open Xunit
+open FsUnit.Xunit
+
+
+[<Fact>]
+let ``increases of sum of sliding window of [199;200;208;210;200;207;240;269;260;263] item is 5`` () =
+    5 |> should equal 5
