1e37c218f7638f187a9b642f6cd95d3fc0409d34
John Nicholas <john.nicholas@dinoroar.io>
2021-12-06 14:06:21 +0000

    up works and example data

--- Day2/CalculatePositionScalar.fs
+++ Day2/CalculatePositionScalar.fs
@@ -15,6 +15,9 @@ let processCommand (state: Position) (input: string) =
     | "down" ->
         { Depth = state.Depth + int (items.[1])
           Horizontal = state.Horizontal }
+    | "up" ->
+        { Depth = state.Depth - int (items.[1])
+          Horizontal = state.Horizontal }
     | _ -> failwith ("invalid command: " + input)
 
 let calculateScalar (position: Position) =
@@ -40,5 +43,26 @@ let ``Forward 5 down 5 should result in 25`` () =
         """forward 5
 down 5
 """
-
     calculatePositionScalar input |> should equal 25
+
+[<Fact>]
+let ``f5d5f8 should result in 65`` () =
+    let input =
+        """forward 5
+down 5
+forward 8
+"""
+    calculatePositionScalar input |> should equal 65
+
+[<Fact>]
+let ``example data should result in 150`` () =
+    let input =
+        """
+forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2
+"""
+    calculatePositionScalar input |> should equal 150
